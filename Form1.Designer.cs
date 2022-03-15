caae22239199e4d79b89d1dcf3c74e4f4853474d
Nathaniel Meyer <nathaniel.meyer@lethbridgecollege.ca>
2022-03-15 09:50:17 -0600

    Changed the form a bit

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -43,52 +43,58 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(130, 15);
+            this.label1.Location = new System.Drawing.Point(173, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.Size = new System.Drawing.Size(124, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Select calling area";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 16);
+            this.label2.Location = new System.Drawing.Point(12, 20);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.Size = new System.Drawing.Size(95, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Length of Call";
             // 
             // lengthBox
             // 
-            this.lengthBox.Location = new System.Drawing.Point(12, 32);
+            this.lengthBox.Location = new System.Drawing.Point(16, 39);
+            this.lengthBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.lengthBox.Name = "lengthBox";
-            this.lengthBox.Size = new System.Drawing.Size(100, 20);
+            this.lengthBox.Size = new System.Drawing.Size(132, 22);
             this.lengthBox.TabIndex = 2;
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(154, 31);
+            this.comboBox1.Location = new System.Drawing.Point(208, 39);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(70, 21);
+            this.comboBox1.Size = new System.Drawing.Size(59, 24);
             this.comboBox1.TabIndex = 4;
             this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(18, 86);
+            this.label3.Location = new System.Drawing.Point(12, 106);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.Size = new System.Drawing.Size(64, 17);
             this.label3.TabIndex = 5;
             this.label3.Text = "Rate/min";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(130, 86);
+            this.label4.Location = new System.Drawing.Point(173, 106);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.Size = new System.Drawing.Size(116, 17);
             this.label4.TabIndex = 6;
             this.label4.Text = "Minimum minutes";
             // 
@@ -96,41 +102,45 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(296, 86);
+            this.label5.Location = new System.Drawing.Point(352, 106);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.Size = new System.Drawing.Size(102, 17);
             this.label5.TabIndex = 7;
             this.label5.Text = "Total Charge";
             // 
             // rateBox
             // 
             this.rateBox.Enabled = false;
-            this.rateBox.Location = new System.Drawing.Point(21, 103);
+            this.rateBox.Location = new System.Drawing.Point(16, 127);
+            this.rateBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rateBox.Name = "rateBox";
-            this.rateBox.Size = new System.Drawing.Size(75, 20);
+            this.rateBox.Size = new System.Drawing.Size(99, 22);
             this.rateBox.TabIndex = 8;
             // 
             // minBox
             // 
             this.minBox.Enabled = false;
-            this.minBox.Location = new System.Drawing.Point(142, 103);
+            this.minBox.Location = new System.Drawing.Point(189, 127);
+            this.minBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.minBox.Name = "minBox";
-            this.minBox.Size = new System.Drawing.Size(75, 20);
+            this.minBox.Size = new System.Drawing.Size(99, 22);
             this.minBox.TabIndex = 9;
             // 
             // totalBox
             // 
             this.totalBox.Enabled = false;
-            this.totalBox.Location = new System.Drawing.Point(267, 102);
+            this.totalBox.Location = new System.Drawing.Point(355, 126);
+            this.totalBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.totalBox.Name = "totalBox";
-            this.totalBox.Size = new System.Drawing.Size(107, 20);
+            this.totalBox.Size = new System.Drawing.Size(99, 22);
             this.totalBox.TabIndex = 10;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(424, 294);
+            this.ClientSize = new System.Drawing.Size(504, 208);
             this.Controls.Add(this.totalBox);
             this.Controls.Add(this.minBox);
             this.Controls.Add(this.rateBox);
@@ -141,6 +151,7 @@
             this.Controls.Add(this.lengthBox);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
